b687bd6bce167a7c9e6b4760ba9fe2a79e1caef7
umars-cloudworld <programmingknowledge03@gmail.com>
2022-03-08 13:08:55 +0500

    function deployment

---
+++ lambda_function.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+def lambda_handler(event,context):
+    d = {'col1':[1,2,3],'col2':[3,4,5]}
+    df = pd.DataFrame(data=d)
+    print(df)
+    print("Done x1.2")
